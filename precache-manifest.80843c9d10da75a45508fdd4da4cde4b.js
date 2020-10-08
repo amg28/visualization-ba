14bda70410ef2ff6149580dd67034ee201f5c6ac
ageek19 <amg2896@gmail.com>
2020-10-08 18:38:58 +0300

    Updates

---
+++ precache-manifest.80843c9d10da75a45508fdd4da4cde4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35372648656d9e217c9a9bd7a0057c04",
+    "url": "/visualization-ba/index.html"
+  },
+  {
+    "revision": "253725d9cce4ab8e63ce",
+    "url": "/visualization-ba/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63f7a07f2fe198f9a93f",
+    "url": "/visualization-ba/static/js/2.e39f9d17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/visualization-ba/static/js/2.e39f9d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253725d9cce4ab8e63ce",
+    "url": "/visualization-ba/static/js/main.a502dd15.chunk.js"
+  },
+  {
+    "revision": "fd967201d368d00fbd2c",
+    "url": "/visualization-ba/static/js/runtime-main.cdc9f7c0.js"
+  }
+]);
